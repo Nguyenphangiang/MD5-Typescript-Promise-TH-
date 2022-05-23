11a29c36ae1b1f3c95305a5503435092f30a3ce6
Nguyenphangiang <phancruel@gmail.com>
2022-05-23 10:22:34 +0700

    up

---
+++ dist/main.js
@@ -0,0 +1,20 @@
+let money = 1000;
+const buyACar = (car) => {
+    return new Promise(((resolve, reject) => {
+        setTimeout(() => {
+            if (money >= 1000) {
+                resolve("Can buy " + car);
+            }
+            else {
+                reject("Do not enough money");
+            }
+        }, 100);
+    }));
+};
+money = 1001;
+const promise = buyACar("Vinfast").then(value => {
+    console.log(value);
+}, error => {
+    console.log(error);
+});
+//# sourceMappingURL=main.js.map
